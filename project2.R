9ecff69c6ced9a3e6d47e0446f0cbf2ad3e765f5
Spencer Clark <slc552@nau.edu>
2020-02-19 18:35:01 -0700

    Completed functions(some errors) + started data analysis

--- project2.R
+++ project2.R
@@ -4,7 +4,22 @@ library(ggplot2)
 
 
 k_fold_cv <- function(x_mat, y_vec, compute_predictions, fold_vec) {
+    error_vec <- vector(length = length(unique(fold_vec)))
 
+    for (fold in unique(fold_vec)) {
+        x_new <- x_mat[fold_vec == fold, ]
+        y_new <- y_vec[fold_vec == fold]
+
+        x_train <- x_mat[fold_vec != fold, ]
+        y_train <- y_vec[fold_vec != fold]
+
+        pred_new <- compute_predictions(x_train, y_train, x_new)
+
+        loss <- (y_new == pred_new) / length(y_new)
+
+        error_vec[fold] <- loss
+    }
+    return(error_vec)
 }
 
 
@@ -14,14 +29,35 @@ nearest_neighbors_cv <- function(x_mat, y_vec, x_new,
     error_mat <- matrix(num_folds, max_neighbors)
     mean_error_vec <- vector(length = max_neighbors)
 
-    for (i in 1:max_neighbors) {
+    for (num_neighbors in 1:max_neighbors) {
         error <- k_fold_cv(x_mat, y_vec,
             compute_predictions =
-                class::knn(i), validation_fold_vec
-        )
-        error_mat[, i] <- error
+                class::knn(x_mat, x_new, y_vec[, 1], k = num_neighbors),
+                                                validation_fold_vec)
+        error_mat[, num_neighbors] <- error
     }
     mean_error_vec <- colMeans(error_mat)
     best_neighbors <- min(mean_error_vec)
 
+    best_pred <- class::knn(x_mat, y_vec, k = best_neighbors)
+    best_x_new <- best_pred %*% x_new
+
+    results_list <- list(best_x_new, mean_error_vec, error_mat)
+    names(results_list) <- c("best", "mean_error_vec", "error_mat")
+
+    return(results_list)
 }
+
+
+spam_datatable <- data.table::fread("spam.data")
+
+x <- spam_datatable[, -58]
+x_scale <- scale(x)
+
+y <- spam_datatable[, 58]
+
+result <- nearest_neighbors_cv(x_scale, y, vector(length = ncol(x)))
+ggplot() +
+  geom_line(aes(
+    neighbors, error.percent, group = validation.fold),
+    data = result$mean_error_vec)
